518093191f1bb0db85f8b9b71e3dc3cfddf226c8
dunitas <92960353+dunitas@users.noreply.github.com>
2021-11-28 12:30:57 -0500

    1) Add comments for vehicle header and 2) Beginning and ending # stmts This was copied from previous assignment change publication to vehicle and comments appropriately to reflect new header for this assignment

--- Vehicle.h
+++ Vehicle.h
@@ -1,5 +1,36 @@
+/*********************************************************************************************************************************/
+/*  David Dresdow                                                                                                                */
+/*                                                                                                                               */
+/*  CIS 1202 501                                                                                                                 */
+/*                                                                                                                               */
+/*  November 28, 2021                                                                                                            */
+/*                                                                                                                               */
+/* Vehicle Base Class Header file - Assignment 14 - Working with base and derived classes                                        */
+/*                                                                                                                               */
+/*  This defines the header file for the class Vehicle                                                                           */
+/*                                                                                                                               */
+/********************************************************************************************************************************/
+
+#ifndef VEHICLE_H
+#define VEHICLE_H
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
 class Vehicle
 {
+
+
+
+
+
+
 };
 
+#endif // !VEHICLE_H
