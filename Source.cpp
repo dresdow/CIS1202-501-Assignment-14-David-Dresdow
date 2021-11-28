ecb74435e9deb3dc515a0eee1ffa695447294623
dunitas <92960353+dunitas@users.noreply.github.com>
2021-11-28 14:13:32 -0500

    Now add comments to the main program and also add necessary # stmts,  I copied these from another program,  change appropriately

---
+++ Source.cpp
@@ -0,0 +1,38 @@
+/*********************************************************************************************************************************/
+/*  David Dresdow                                                                                                                */
+/*                                                                                                                               */
+/*  CIS 1202 501                                                                                                                 */
+/*                                                                                                                               */
+/*  November 28, 2021                                                                                                            */
+/*                                                                                                                               */
+/*  Program Name - Assignment 14 - Create and Test Base and Derived  Classes                                                     */
+/*                                                                                                                               */
+/* This program will allow users to:                                                                                             */
+/*   1) Enter and display information which goes to the base class Vehicle ( manufacturer and year built)                        */
+/*   2) Enter and display information which goes to the derived class Car (manufacturer, year built and number of doors)         */
+/*   3) Enter and display information which goes to the derived class Truck (manufacturer, year built and towing capacity)       */
+/*                                                                                                                               */
+/********************************************************************************************************************************/
+
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include "Vehicle.h"
+#include "Car.h"
+#include "Truck.h"
+
+
+using namespace std;
+
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
