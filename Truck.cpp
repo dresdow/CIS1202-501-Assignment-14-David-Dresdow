a46c1cee57f5f3e1662da65030426cacaf4acccd
dunitas <92960353+dunitas@users.noreply.github.com>
2021-11-28 16:01:07 -0500

    Next define the methods for the truck derived class.   I will simply copy the car.cpp this will be so similar,  I just have to change a few things related to changing number of doors to towing capacity

--- Truck.cpp
+++ Truck.cpp
@@ -1,1 +1,56 @@
+/*********************************************************************************************************************************/
+/*  David Dresdow                                                                                                                */
+/*                                                                                                                               */
+/*  CIS 1202 501                                                                                                                 */
+/*  November 28, 2021                                                                                                            */
+/*                                                                                                                               */
+/*  Truck Class Implementation  file - Assignment 14 -   ( class declaration in Truck header file)                               */
+/*                                                                                                                               */
+/*  This defines the member definitions in a  file for the class Truck (a derived class from base class Vehicle)                 */
+/*                                                                                                                               */
+/********************************************************************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <iomanip>
+#include <cstdlib>
+#include "Vehicle.h"
 #include "Truck.h"
+
+using namespace std;
+
+int Truck::getTowingCapacity() const
+{
+	return Truck::towingCapacity;
+}
+
+void Truck::setTowingCapacity(int towingCap)
+{
+	Truck::towingCapacity = towingCap;
+}
+
+void Truck::displayInfo()
+
+{
+	Vehicle::displayInfo();
+
+	cout << "\nTowing capacity: " << Truck::towingCapacity;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
